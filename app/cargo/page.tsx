91043ada6f41982a133ff7cf9cf72be879dc8762
Saroj Adhikari <sarojadh1111@gmail.com>
2026-03-04 21:19:22 +0545

    Cargo

--- app/cargo/page.tsx
+++ app/cargo/page.tsx
@@ -1,4 +1,35 @@
+import Container from "../components/container";
+import Items from "../components/items";
+import { IconName } from "lucide-react/dynamic";
+
 const CargoManagement = () => {
-  return <>Cargo Management</>;
+  interface CargoManagement {
+    name: string;
+    description: string;
+    icon: IconName;
+    route: string;
+  }
+
+  const CargoManagement: CargoManagement[] = [
+    {
+      name: "Cargo Received",
+      description: "Record incoming cargo details",
+      icon: "package",
+      route: "cargo-received",
+    },
+    {
+      name: "Cargo Send",
+      description: "Manage outgoing cargo dispatch",
+      icon: "truck",
+      route: "cargo-send",
+    },
+  ];
+
+  return (
+    <Container>
+      <Items clients={CargoManagement} cols={2} />
+    </Container>
+  );
 };
+
 export default CargoManagement;
